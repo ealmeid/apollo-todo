74d0820ef1a69ed0ea8e4f04a0bf775e79c89b97
Evan Almeida <evan.almeidaf@gmail.com>
2024-01-14 17:27:37 -0500

    💎 style(ui): create navbar component

---
+++ src/components/custom/navbar.tsx
@@ -0,0 +1,48 @@
+import NextLink from "next/link";
+import {
+  GanttChartSquare,
+  HomeIcon,
+  Info,
+  User2,
+  UserRoundX,
+} from "lucide-react";
+import { useAuth } from "@clerk/nextjs";
+import {
+  TooltipProvider,
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  Text,
+} from "@/components";
+
+export const NavBar = () => {
+  const { isLoaded, isSignedIn } = useAuth();
+
+  return (
+    <div className="bg-gray-200 rounded-2xl p-3 px-6 flex gap-8 m-auto max-w-max shadow-md">
+      <NextLink href="/">
+        <HomeIcon />
+      </NextLink>
+      <GanttChartSquare />
+      {isSignedIn ? (
+        <NextLink href="/profile">
+          <User2 />
+        </NextLink>
+      ) : (
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <NextLink href="/login">
+                <UserRoundX />
+              </NextLink>
+            </TooltipTrigger>
+            <TooltipContent>
+              <Text as="p">Login or Signup</Text>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      )}
+      <Info />
+    </div>
+  );
+};
