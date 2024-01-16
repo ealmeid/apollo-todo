80345de8b0367d2d2437ba69be3a1919ff40bf0d
Evan Almeida <evan.almeidaf@gmail.com>
2024-01-16 17:45:33 -0500

    ✨ feat(graphql): add graphql schema

---
+++ src/pages/api/graphql/index.ts
@@ -0,0 +1,10 @@
+import { listResolvers, listTypeDefs } from "./list";
+import { todoResolvers, todoTypeDefs } from "./todo";
+import { userResolvers, userTypeDefs } from "./user";
+
+import { makeExecutableSchema } from "@graphql-tools/schema";
+
+export const schema = makeExecutableSchema({
+  typeDefs: [userTypeDefs, listTypeDefs, todoTypeDefs],
+  resolvers: [userResolvers, listResolvers, todoResolvers],
+});
