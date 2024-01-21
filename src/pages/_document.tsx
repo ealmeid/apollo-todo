9659084c932f587e9a7ff75ce2a1b034a7a5b004
Evan Almeida <evan.almeidaf@gmail.com>
2024-01-21 15:31:38 -0500

    💎 style(font): fix inter font not being applied to dialog component
    add the inter font variable to the global html component since the dialog component gets rendered
    outside of the main tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { fontSans } from "./_app";
+
+import { cn } from "@/lib/utils";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className={cn(fontSans.variable)}>
       <Head />
       <body>
         <Main />
