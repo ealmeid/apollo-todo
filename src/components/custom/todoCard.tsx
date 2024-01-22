e46a874975022f2dc4603e43cf51b249a306dde8
Evan Almeida <evan.almeidaf@gmail.com>
2024-01-22 01:12:04 -0500

    ✨ feat(task): show all tasks for a user, and add ability to create a task on home page
    Also tweaked the todoCard to be a forward ref and created a MotionTodoCard so it can be animated on
    creation & deletion of a task.

--- src/components/custom/todoCard.tsx
+++ src/components/custom/todoCard.tsx
@@ -14,86 +14,102 @@ import {
   Text,
 } from "..";
 import { Trash2 } from "lucide-react";
+import { motion } from "framer-motion";
 
-export interface TodoCardProps extends React.HTMLAttributes<HTMLDivElement> {
+export interface TodoCardProps {
   id: string;
   name: string;
 }
 
-export const TodoCard = ({ id, name, ...props }: TodoCardProps) => {
-  const [selectedLists, setSelectedLists] = useState<string[]>([]);
+// eslint-disable-next-line
+const TodoCardComponent = React.forwardRef<HTMLDivElement, TodoCardProps>(
+  ({ id, name, ...props }, ref) => {
+    const [selectedLists, setSelectedLists] = useState<string[]>([]);
 
-  const [addTaskToLists] = useAddTasksToListsMutation();
+    const [addTaskToLists] = useAddTasksToListsMutation();
 
-  const [deleteTask] = useDeleteTaskMutation({
-    variables: {
-      id,
-    },
-    update(cache) {
-      cache.evict({ id: cache.identify({ __typename: "Task", id }) });
-    },
-  });
+    const [deleteTask] = useDeleteTaskMutation({
+      variables: {
+        id,
+      },
+      update(cache) {
+        cache.evict({ id: cache.identify({ __typename: "Task", id }) });
+      },
+    });
 
-  const { data } = useGetListsByUserQuery({
-    onCompleted: ({ getListsByUser }) => {
-      const listIds = getListsByUser
-        .filter((list) => list.taskIds.includes(id))
-        .map((list) => list.id);
-      setSelectedLists(listIds);
-    },
-  });
+    const { data } = useGetListsByUserQuery({
+      onCompleted: ({ getListsByUser }) => {
+        const listIds = getListsByUser
+          .filter((list) => list.taskIds.includes(id))
+          .map((list) => list.id);
+        setSelectedLists(listIds);
+      },
+    });
 
-  return (
-    <Dialog>
-      <DialogTrigger>
-        <div
-          key={id}
-          className="bg-slate-100 flex items-center gap-2 w-full px-6 py-4 border-slate-200 border rounded-md min-h-16 hover:bg-slate-50"
-        >
-          <Checkbox className="w-6 h-6" onClick={(e) => e.stopPropagation()} />
-          <div>{name}</div>
-        </div>
-      </DialogTrigger>
-      <DialogContent className="p-6">
-        <div className="flex flex-col gap-6 max-h-[300px] p-4 overflow-y-auto">
-          <Text as="h3">{name}</Text>
-          <Button
-            variant="destructive"
-            className="flex gap-2"
-            onClick={() => deleteTask()}
+    return (
+      <Dialog>
+        <DialogTrigger>
+          <div
+            ref={ref}
+            key={id}
+            className="bg-slate-100 flex items-center gap-2 w-full px-6 py-4 border-slate-200 border rounded-md min-h-16 hover:bg-slate-50"
           >
-            <Trash2 className="w-4 h-4">Delete Task</Trash2>
-            Delete Task
-          </Button>
-          <Text as="p">Created:</Text>
-          {data?.getListsByUser.map((list) => (
-            <div
-              key={list.id}
-              className="shadow-md p-4 flex items-center gap-6 rounded-md"
+            <Checkbox
+              className="w-6 h-6"
+              onClick={(e) => e.stopPropagation()}
+            />
+            <div>{name}</div>
+          </div>
+        </DialogTrigger>
+        <DialogContent className="p-6">
+          <div className="flex flex-col gap-6 max-h-[300px] p-4 overflow-y-auto">
+            <Text as="h3">{name}</Text>
+            <Button
+              variant="destructive"
+              className="flex gap-2"
+              onClick={() => deleteTask()}
             >
-              {list.title}
-              <Checkbox
-                checked={selectedLists.includes(list.id)}
-                onClick={() => setSelectedLists([...selectedLists, list.id])}
-              />
-            </div>
-          ))}
-        </div>
-        <Button
-          onClick={() => {
-            addTaskToLists({
-              variables: {
-                taskIds: [id],
-                listIds: selectedLists,
-              },
-            });
-          }}
-        >
-          Add Tasks to Lists
-        </Button>
-      </DialogContent>
-    </Dialog>
-  );
+              <Trash2 className="w-4 h-4">Delete Task</Trash2>
+              Delete Task
+            </Button>
+            <Text as="p">Created:</Text>
+            {data?.getListsByUser.map((list) => (
+              <div
+                key={list.id}
+                className="shadow-md p-4 flex items-center gap-6 rounded-md"
+              >
+                {list.title}
+                <Checkbox
+                  checked={selectedLists.includes(list.id)}
+                  onClick={() => setSelectedLists([...selectedLists, list.id])}
+                />
+              </div>
+            ))}
+          </div>
+          <Button
+            onClick={() => {
+              addTaskToLists({
+                variables: {
+                  taskIds: [id],
+                  listIds: selectedLists,
+                },
+              });
+            }}
+          >
+            Add Tasks to Lists
+          </Button>
+        </DialogContent>
+      </Dialog>
+    );
+  }
+);
+
+export const MotionTodoCard = motion(TodoCardComponent);
+
+export const TodoCard = TodoCardComponent as React.ForwardRefExoticComponent<
+  TodoCardProps & React.RefAttributes<HTMLDivElement>
+> & {
+  Skeleton: React.FC;
 };
 
 // eslint-disable-next-line
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,36 @@
-import { Button, Text, Input } from "@/components";
+import { useState } from "react";
+import { Button, Text, Input, TodoCard, MotionTodoCard } from "@/components";
+import {
+  GetTasksByUserDocument,
+  GetTasksByUserQuery,
+  useCreateTaskMutation,
+  useGetTasksByUserQuery,
+} from "@/graphql/types/client";
+import { AnimatePresence } from "framer-motion";
 
-export default function Home() {
+export const Home = () => {
+  const [todoValue, setTodoValue] = useState("");
+
+  const { data, loading, error } = useGetTasksByUserQuery();
+
+  const [createTodo] = useCreateTaskMutation({
+    update(cache, { data }) {
+      const newTodo = data?.createTask;
+      const existingTasks = cache.readQuery<GetTasksByUserQuery>({
+        query: GetTasksByUserDocument,
+      });
+
+      const newArr = [newTodo, ...(existingTasks?.getTasksByUser ?? [])];
+
+      cache.writeQuery({
+        query: GetTasksByUserDocument,
+        data: {
+          getTasksByUser: newArr,
+        },
+      });
+    },
+  });
+
   return (
     <div className="items-center flex flex-col gap-8 m-auto mt-24">
       <Text as="h1">apollo todo</Text>
@@ -8,9 +38,56 @@ export default function Home() {
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
       </Text>
       <div className="flex gap-2">
-        <Input placeholder="Pick up pasta..." />
-        <Button>Add</Button>
+        <Input
+          value={todoValue}
+          onChange={(e) => setTodoValue(e.currentTarget.value)}
+          placeholder="Pick up pasta..."
+        />
+        <Button
+          onClick={() => {
+            createTodo({
+              variables: {
+                title: todoValue,
+              },
+            });
+          }}
+        >
+          Add
+        </Button>
       </div>
+      {loading && (
+        <div className="flex flex-col gap-4 max-w-[300px] w-full">
+          {Array.from({ length: 3 }, (_, i) => (
+            <TodoCard.Skeleton key={i} />
+          ))}
+        </div>
+      )}
+
+      {!loading &&
+        !error &&
+        data &&
+        (data?.getTasksByUser.length === 0 ? (
+          <Text as="p">No todos yet!</Text>
+        ) : (
+          <div className="flex flex-col gap-4 max-w-[300px] w-full">
+            <AnimatePresence mode={"popLayout"}>
+              {data?.getTasksByUser.map((todo) => (
+                <MotionTodoCard
+                  key={todo.id}
+                  id={todo.id}
+                  name={todo.title}
+                  layout
+                  // initial={{ scale: 0.8, opacity: 0 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  exit={{ scale: 0.8, opacity: 0 }}
+                  transition={{ type: "spring" }}
+                />
+              ))}
+            </AnimatePresence>
+          </div>
+        ))}
     </div>
   );
-}
+};
+
+export default Home;
