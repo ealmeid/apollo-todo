c209527a26af9f81acee5e81c1f7b091d4f3b7b1
Evan Almeida <evan.almeidaf@gmail.com>
2024-01-29 22:11:39 -0500

    🧹 chore(page): add default UI message when user has no lists

--- src/pages/manage/index.tsx
+++ src/pages/manage/index.tsx
@@ -13,6 +13,9 @@ export const Manage = () => {
     <div className="items-center flex flex-col gap-8 m-auto mt-24">
       <Text as="h1">Your Lists</Text>
       <div className="flex flex-col gap-2 max-w-[300px] w-full">
+        {data?.getListsByUser.length === 0 && (
+          <div className="m-auto">No Lists yet!</div>
+        )}
         {data?.getListsByUser.map((list) => (
           <div className="p-4 shadow-md w-full rounded-lg" key={list.id}>
             {list.title}
