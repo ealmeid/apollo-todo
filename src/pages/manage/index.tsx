8ab1f6630c875c94b87000d0d26a3fa714784972
Evan Almeida <evan.almeidaf@gmail.com>
2024-02-07 18:38:08 -0500

    ✨ feat(list): update UI of list cards, add color override for Progress component

--- src/pages/manage/index.tsx
+++ src/pages/manage/index.tsx
@@ -1,4 +1,4 @@
-import { Text, Button } from "@/components";
+import { Text, Button, Progress } from "@/components";
 import {
   useCreateListMutation,
   useGetListsByUserQuery,
@@ -12,16 +12,6 @@ export const Manage = () => {
   return (
     <div className="items-center flex flex-col gap-8 m-auto mt-24">
       <Text as="h1">Your Lists</Text>
-      <div className="flex flex-col gap-2 max-w-[300px] w-full">
-        {data?.getListsByUser.length === 0 && (
-          <div className="m-auto">No Lists yet!</div>
-        )}
-        {data?.getListsByUser.map((list) => (
-          <div className="p-4 shadow-md w-full rounded-lg" key={list.id}>
-            {list.title}
-          </div>
-        ))}
-      </div>
       <Button
         onClick={() =>
           createList({
@@ -33,6 +23,36 @@ export const Manage = () => {
       >
         Create List
       </Button>
+      <div className="flex flex-col gap-4 max-w-[300px] w-full">
+        {data?.getListsByUser.length === 0 && (
+          <div className="m-auto">No Lists yet!</div>
+        )}
+        {data?.getListsByUser.map((list) => (
+          <div
+            className="cursor-pointer bg-slate-100 flex items-center gap-4 w-full px-6 py-4 border-slate-200 border rounded-lg min-h-16 hover:bg-slate-50"
+            key={list.id}
+          >
+            <div className="flex items-start gap-4 w-full">
+              <div className="min-w-8 rounded-md bg-white border-slate-400 border w-8 h-8 flex items-center justify-center">
+                {list.title.substring(0, 1).toUpperCase()}
+              </div>
+              <div className="flex flex-col gap-1 overflow-hidden">
+                <Text as="p" className="!m-0 font-semibold">
+                  {list.title}
+                </Text>
+                <Text as="muted" className="truncate mb-2">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </Text>
+                <Progress
+                  value={50}
+                  className="h-2 border"
+                  bgColor="bg-green-800"
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
